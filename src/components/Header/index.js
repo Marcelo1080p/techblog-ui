a0638994b78c4b5be1e72324d239052b945dbb40
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-18 15:12:24 -0300

    input pesquisa criado e adicionado ao header

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import InputPesquisa from "../InputPesquisa";
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <h2>Logo</h2>
+                <InputPesquisa placeholder="Pesquisar..."/>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/InputPesquisa/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./InputPesquisa.css";
+
+const InputPesquisa = (props) => {
+    return (
+        <div className="input-pesquisa">
+            <input placeholder={props.placeholder}/>
+            <p className="lupa-pesquisa">
+                <p className="circulo-pesquisa"></p>
+            </p>
+        </div>
+    )
+};
+
+export default InputPesquisa;
