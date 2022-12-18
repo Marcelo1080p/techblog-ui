e1830d460da0c73fb40d14752466d21f16e44493
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-18 15:15:14 -0300

    diminuição feita no tamanho d alupa do input pesquisa

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import InputPesquisa from "../InputPesquisa";
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <header>
-            <div>
-                <h2>Logo</h2>
-                <InputPesquisa placeholder="Pesquisar..."/>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div>
+        <h2>Logo</h2>
+        <InputPesquisa placeholder="Pesquisar..." />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
